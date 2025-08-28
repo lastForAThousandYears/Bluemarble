fc253116b78cf7dbf32ce1ac4762582b72ea1ffa
HAJONK <169044023+HAJONK@users.noreply.github.com>
2025-08-28 21:41:57 +0930

    Testing, Testing, Testing/ Thanks Lastforathousandyears

--- src/BlueMarble.meta.js
+++ src/BlueMarble.meta.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name         Blue Marble For Us Furries
-// @namespace    https://github.com/IonTheProtogen/
-// @version      0.85.3
+// @name         Blue Marble
+// @namespace    https://github.com/SwingTheVine/
+// @version      0.85.0
 // @description  A userscript to automate and/or enhance the user experience on Wplace.live. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with Wplace.live in any way, use at your own risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under the MPL-2.0 license. The "Blue Marble" icon is licensed under CC0 1.0 Universal (CC0 1.0) Public Domain Dedication. The image is owned by NASA.
-// @author       SwingTheVine And Furries :3
+// @author       SwingTheVine
 // @license      MPL-2.0
 // @supportURL   https://discord.gg/tpeBPy46hf
 // @homepageURL  https://bluemarble.camilledaguin.fr/
 // @icon         https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/051271c433a42db968a865b00f81bb979ee7d13f/dist/assets/Favicon.png
-// @updateURL    https://raw.githubusercontent.com/IonTheProtogen/Bluemarble_Furries_uwu/refs/heads/master/dist/BlueMarble.user.js
-// @downloadURL  https://raw.githubusercontent.com/IonTheProtogen/Bluemarble_Furries_uwu/refs/heads/master/dist/BlueMarble.user.js
+// @updateURL    https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/main/dist/BlueMarble.user.js
+// @downloadURL  https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/main/dist/BlueMarble.user.js
 // @match        https://wplace.live/*
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
@@ -22,4 +22,3 @@
 
 // Wplace  --> https://wplace.live
 // License --> https://www.mozilla.org/en-US/MPL/2.0/
-
